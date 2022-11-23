1d9e45038180b6bfe5da709afcc8703af4ae25b2
v-v-v79 <vivedesh@gmail.com>
2022-11-23 06:53:47 -0800

    Favorites Upload Slava

--- src/test/java/com/trycloud/step_definitions/DeleteFileSteps_Slava.java
+++ src/test/java/com/trycloud/step_definitions/DeleteFileSteps_Slava.java
@@ -1,6 +1,6 @@
 package com.trycloud.step_definitions;
 
-import com.trycloud.pages.DeleteFilesPage_Slava;
+import com.trycloud.pages.FilesModulePage;
 import com.trycloud.pages.LoginPage;
 import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.Driver;
@@ -9,15 +9,17 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 
 public class DeleteFileSteps_Slava {
 
     LoginPage loginPage = new LoginPage();
-    DeleteFilesPage_Slava deleteFiles_Page_slava = new DeleteFilesPage_Slava();
+    FilesModulePage filesModulePage = new FilesModulePage();
     Actions actions = new Actions(Driver.getDriver());
     String expectedFileName = "";
     String actualFileName = "";
+
 
     @Given("user on the dashboard page")
     public void user_on_the_dashboard_page() {
@@ -35,37 +37,37 @@ public class DeleteFileSteps_Slava {
     @When("user click action-icon from any file on the page")
     public void user_click_action_icon_from_any_file_on_the_page() {
         BrowserUtils.sleep(3);
-        BrowserUtils.hover(deleteFiles_Page_slava.firstFileName);
+        BrowserUtils.hover(filesModulePage.firstFileName);
         BrowserUtils.sleep(3);
-        actualFileName = deleteFiles_Page_slava.firstFileName.getText();
-        BrowserUtils.hover(deleteFiles_Page_slava.firstFileActionsMenu);
-        deleteFiles_Page_slava.firstFileActionsMenu.click();
+        actualFileName = filesModulePage.firstFileName.getText();
+        BrowserUtils.hover(filesModulePage.firstFileActionsMenu);
+        filesModulePage.firstFileActionsMenu.click();
         BrowserUtils.sleep(3);
     }
     @And("user choose the Delete file option")
     public void userChooseTheDeleteFileOption() {
         BrowserUtils.sleep(3);
-        actions.moveToElement(deleteFiles_Page_slava.fileDeleteButton)
+        actions.moveToElement(filesModulePage.fileDeleteButton)
                 .pause(2).click().pause(2).perform();
     }
 
     @When("the user clicks the Deleted files sub-module on the left side")
     public void theUserClicksTheDeletedFilesSubModuleOnTheLeftSide() {
-        BrowserUtils.hover(deleteFiles_Page_slava.deletedFilesMenu);
-        deleteFiles_Page_slava.deletedFilesMenu.click();
+        BrowserUtils.hover(filesModulePage.deletedFilesMenu);
+        filesModulePage.deletedFilesMenu.click();
         BrowserUtils.waitForPageToLoad(30);
     }
 
     @Then("Verify the deleted file is displayed on the page.")
     public void verify_the_deleted_file_is_displayed_on_the_page() {
         BrowserUtils.sleep(2);
-        actions.moveToElement(deleteFiles_Page_slava.sortDeleted)
+        actions.moveToElement(filesModulePage.sortDeleted)
                 .pause(1).click().pause(2).click().pause(1).perform();
         BrowserUtils.sleep(2);
-        expectedFileName = deleteFiles_Page_slava.lastDeletedFileName.getText();
+        expectedFileName = filesModulePage.lastDeletedFileName.getText();
         BrowserUtils.sleep(2);
         Assert.assertEquals(expectedFileName, actualFileName);
-        actions.moveToElement(deleteFiles_Page_slava.restoreButton)
+        actions.moveToElement(filesModulePage.restoreButton)
                 .pause(1).click().pause(1).perform();
 
     }
